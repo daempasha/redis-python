a6923a34f8e72fde59d0de05fed9f87e77a0376b
Daem Pasha <daempasha@gmail.com>
2022-11-27 16:55:54 +0000

    Return PONG to PING requests

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection, address = server_socket.accept()  # wait for client
+    connection.recv(1024)
+    connection.send("+PONG\r\n")
 
 
 if __name__ == "__main__":
