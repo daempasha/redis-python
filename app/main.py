42847482f6265032e4960eb7c0c26962a4894c1d
Daem Pasha <daempasha@gmail.com>
2022-11-27 16:56:43 +0000

    Format string as bytes

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, address = server_socket.accept()  # wait for client
     connection.recv(1024)
-    connection.send("+PONG\r\n")
+    connection.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
